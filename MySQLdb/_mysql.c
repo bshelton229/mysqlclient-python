952b7d08d9776720cc0adf8e689874a0d3b8120e
Bryan Shelton <bryan@rover.com>
2019-09-28 12:19:46 -0700

    [disable-ssl] Disable ssl if no parameters are sent

--- MySQLdb/_mysql.c
+++ MySQLdb/_mysql.c
@@ -478,8 +478,16 @@ _mysql_ConnectionObject_Initialize(
     if (local_infile != -1)
         mysql_options(&(self->connection), MYSQL_OPT_LOCAL_INFILE, (char *) &local_infile);
 
+
+
     if (ssl) {
         mysql_ssl_set(&(self->connection), key, cert, ca, capath, cipher);
+    } else {
+#ifdef SSL_MODE_DISABLED
+        /* Force the client to disable SSL connections */
+        unsigned int ssl_mode_disabled = SSL_MODE_DISABLED;
+        mysql_options(&(self->connection), MYSQL_OPT_SSL_MODE, (char *) &ssl_mode_disabled);
+#endif
     }
 
     conn = mysql_real_connect(&(self->connection), host, user, passwd, db,
